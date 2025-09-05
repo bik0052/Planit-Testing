a07e24294b3d6b99c491fb0c53db3e64c5f8b9cd
bik0052 <42190410+bik0052@users.noreply.github.com>
2025-09-05 16:43:15 +1200

    Update BaseTest.java

--- src/main/java/com/planit/base/BaseTest.java
+++ src/main/java/com/planit/base/BaseTest.java
@@ -1,20 +1,28 @@
 package com.planit.base;
 
-import com.planit.config.ConfigReader;
-import com.planit.utils.DriverFactory;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
     protected WebDriver driver;
-    protected ConfigReader config = new ConfigReader();
 
     @BeforeMethod
     public void setUp() {
-        driver = DriverFactory.createDriver(config.getBrowser(), config.isHeadless());
+        ChromeOptions options = new ChromeOptions();
+
+        // Headless for CI by default
+        if (Boolean.parseBoolean(System.getProperty("headless", "true"))) {
+            options.addArguments("--headless=new");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--window-size=1920,1080");
+        }
+
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
-        driver.get(config.getBaseUrl());
+        driver.get("http://jupiter.cloud.planittesting.com");
     }
 
     @AfterMethod
