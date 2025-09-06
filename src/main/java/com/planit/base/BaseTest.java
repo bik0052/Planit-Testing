9bb2c95641715500edc9ec5ca5279ce3fccc3a01
bik0052 <42190410+bik0052@users.noreply.github.com>
2025-09-06 14:39:46 +1200

    Update BaseTest.java

--- src/main/java/com/planit/base/BaseTest.java
+++ src/main/java/com/planit/base/BaseTest.java
@@ -1,31 +1,38 @@
 package com.planit.base;
 
+import com.planit.config.ConfigReader;
+import com.planit.utils.DriverFactory;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import java.time.Duration;
 
 public class BaseTest {
     protected WebDriver driver;
+    protected WebDriverWait wait;
+    protected ConfigReader config;
 
-    @BeforeMethod
+    @BeforeMethod(alwaysRun = true)
     public void setUp() {
-        ChromeOptions options = new ChromeOptions();
+        config = new ConfigReader();
+        driver = DriverFactory.createDriver(config.getBrowser(), config.isHeadless());
 
-        // Headless for CI by default
-        if (Boolean.parseBoolean(System.getProperty("headless", "true"))) {
-            options.addArguments("--headless=new");
-            options.addArguments("--disable-gpu");
-            options.addArguments("--window-size=1920,1080");
-        }
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(config.getImplicitWait()));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(config.getPageLoadTimeout()));
+        driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(30));
 
-        driver = new ChromeDriver(options);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(config.getExplicitWait()));
+
+        driver.get(config.getBaseUrl());
         driver.manage().window().maximize();
-        driver.get("http://jupiter.cloud.planittesting.com");
+
+        // Minor sanity: wait until Angular ng-view is present
+        ((JavascriptExecutor) driver).executeScript("return document.readyState");
     }
 
-    @AfterMethod
+    @AfterMethod(alwaysRun = true)
     public void tearDown() {
         if (driver != null) {
             driver.quit();
