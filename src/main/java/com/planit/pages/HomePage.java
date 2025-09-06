a91df725599031faaf693ada800de8ae9e6ef6a2
bik0052 <42190410+bik0052@users.noreply.github.com>
2025-09-06 15:35:14 +1200

    Update HomePage.java

--- src/main/java/com/planit/pages/HomePage.java
+++ src/main/java/com/planit/pages/HomePage.java
@@ -1,1 +1,42 @@
 
+package com.planit.pages;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class HomePage {
+    private final WebDriver driver;
+    private final WebDriverWait wait;
+
+    private final By linkContact = By.linkText("Contact");
+    private final By linkShop = By.linkText("Shop");
+    private final By ngView = By.cssSelector("div[ng-view]");
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(ngView));
+    }
+
+    public void goToContactPage() {
+        WebElement el = wait.until(ExpectedConditions.elementToBeClickable(linkContact));
+        scrollIntoView(el);
+        el.click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(ngView));
+    }
+
+    public void goToShopPage() {
+        WebElement el = wait.until(ExpectedConditions.elementToBeClickable(linkShop));
+        scrollIntoView(el);
+        el.click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(ngView));
+    }
+
+    private void scrollIntoView(WebElement element) {
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView({block: 'center'});", element);
+    }
+}
+
